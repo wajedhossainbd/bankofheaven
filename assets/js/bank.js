dde4163c445e385b5e134be6838c41d3d379a029
Wajed Hossain <34432420+wajedhossainbd@users.noreply.github.com>
2022-08-18 07:57:13 +0600

    new v2

--- assets/js/bank.js
+++ assets/js/bank.js
@@ -1,62 +1,127 @@
-// Desposit Script
-document.getElementById('deposit-btn').addEventListener('click', function () {
-    const depositInput = document.getElementById('deposit-input');
-    const depositNewString = depositInput.value;
-    const depositNew = parseFloat(depositNewString);
-    const depositBalance = document.getElementById('deposit');
-    const depositOldString = depositBalance.innerText;
-    const depositOld = parseFloat(depositOldString);
+//// Desposit Script
+//document.getElementById('deposit-btn').addEventListener('click', function () {
+//    const depositInput = document.getElementById('deposit-input');
+//    const depositNewString = depositInput.value;
+//    const depositNew = parseFloat(depositNewString);
+//    const depositBalance = document.getElementById('deposit');
+//    const depositOldString = depositBalance.innerText;
+//    const depositOld = parseFloat(depositOldString);
+//
+//    if (isNaN(depositNew) || depositNew <= 0) {
+//        alert('Input valid Deposit amount')
+//        return;
+//
+//    }
+//    const depositUpdate = depositNew + depositOld;
+//    depositBalance.innerText = depositUpdate.toFixed(2);
+//    depositInput.value = '';
+//
+//    // Balance adjustment area
+//    const balance = document.getElementById('balance');
+//    const balanceOldString = balance.innerText;
+//    const balanceOld = parseFloat(balanceOldString);
+//    const balanceNew = balanceOld + depositNew
+//    balance.innerText = balanceNew.toFixed(2);
+//
+//})
+//
+//// Withdraw Script
+//document.getElementById('withdraw-btn').addEventListener('click', function () {
+//    const withdrawInput = document.getElementById('withdraw-input');
+//    const withdrawNewString = withdrawInput.value;
+//    const withdrawNew = parseFloat(withdrawNewString);
+//    const withdrawBalance = document.getElementById('withdraw');
+//    const withdrawOldString = withdrawBalance.innerText;
+//    const withdrawOld = parseFloat(withdrawOldString);
+//    withdrawInput.value = '';
+//    if (isNaN(withdrawNew) || withdrawNew <= 0) {
+//        alert('Input valid withdraw amount')
+//        return;
+//
+//    }
+//
+//
+//    // Balance adjustment area
+//    const balance = document.getElementById('balance');
+//    const balanceOldString = balance.innerText;
+//    const balanceOld = parseFloat(balanceOldString);
+//
+//
+//    if (withdrawNew > balanceOld) {
+//        alert(
+//            'Insufficient Balance');
+//        return;
+//
+//    }
+//    const withdrawUpdate = withdrawNew + withdrawOld;
+//    withdrawBalance.innerText = withdrawUpdate.toFixed(2);
+//
+//
+//    const balanceNew = balanceOld - withdrawNew
+//    balance.innerText = balanceNew.toFixed(2);
+//})
 
-    if (isNaN(depositNew) || depositNew <= 0) {
+
+// Deposit Section
+
+document.getElementById('dep-btn').addEventListener('click', function () {
+
+    // Wrong input Alert
+
+    if (isNaN((parseFloat(document.getElementById('dep-inp').value))) || (parseFloat(document.getElementById('dep-inp').value)) <= 0) {
         alert('Input valid Deposit amount')
         return;
 
     }
-    const depositUpdate = depositNew + depositOld;
-    depositBalance.innerText = depositUpdate.toFixed(2);
-    depositInput.value = '';
 
-    // Balance adjustment area
-    const balance = document.getElementById('balance');
-    const balanceOldString = balance.innerText;
-    const balanceOld = parseFloat(balanceOldString);
-    const balanceNew = balanceOld + depositNew
-    balance.innerText = balanceNew.toFixed(2);
+    // Deposit Input
+
+    document.getElementById('dep-total').innerText = ((parseFloat(document.getElementById('dep-total').innerText)) + (parseFloat(document.getElementById('dep-inp').value))).toFixed(2);
+
+    // Deposit Balance Adjustment
+
+    document.getElementById('bal-total').innerText = ((parseFloat(document.getElementById('bal-total').innerText)) + (parseFloat(document.getElementById('dep-inp').value))).toFixed(2);
+
+
+    // Deposit input clear
+
+    document.getElementById('dep-inp').value = '';
+
 
 })
 
-// Withdraw Script
-document.getElementById('withdraw-btn').addEventListener('click', function () {
-    const withdrawInput = document.getElementById('withdraw-input');
-    const withdrawNewString = withdrawInput.value;
-    const withdrawNew = parseFloat(withdrawNewString);
-    const withdrawBalance = document.getElementById('withdraw');
-    const withdrawOldString = withdrawBalance.innerText;
-    const withdrawOld = parseFloat(withdrawOldString);
-    withdrawInput.value = '';
-    if (isNaN(withdrawNew) || withdrawNew <= 0) {
-        alert('Input valid withdraw amount')
-        return;
-
-    }
+// Withdraw Section
 
 
-    // Balance adjustment area
-    const balance = document.getElementById('balance');
-    const balanceOldString = balance.innerText;
-    const balanceOld = parseFloat(balanceOldString);
+document.getElementById('wtd-btn').addEventListener('click', function () {
 
+    // Insufficient balance Alert
 
-    if (withdrawNew > balanceOld) {
+    if ((parseFloat(document.getElementById('wtd-inp').value)) > (parseFloat(document.getElementById('bal-total').innerText))) {
         alert(
             'Insufficient Balance');
         return;
 
     }
-    const withdrawUpdate = withdrawNew + withdrawOld;
-    withdrawBalance.innerText = withdrawUpdate.toFixed(2);
 
+    // Wrong input Alert
 
-    const balanceNew = balanceOld - withdrawNew
-    balance.innerText = balanceNew.toFixed(2);
+    if (isNaN((parseFloat(document.getElementById('wtd-inp').value))) || (parseFloat(document.getElementById('wtd-inp').value)) <= 0) {
+        alert('Input valid Deposit amount')
+        return;
+
+    }
+
+    // Withdraw Input
+
+    document.getElementById('wtd-total').innerText = ((parseFloat(document.getElementById('wtd-total').innerText)) + (parseFloat(document.getElementById('wtd-inp').value))).toFixed(2);
+
+    // Withdraw Balance Adjustment
+
+    document.getElementById('bal-total').innerText = ((parseFloat(document.getElementById('bal-total').innerText)) - (parseFloat(document.getElementById('wtd-inp').value))).toFixed(2);
+
+    // Withdraw input clear
+
+    document.getElementById('wtd-inp').value = '';
+
 })
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,10 +1,23 @@
-document.getElementById('login-btn').addEventListener('click', function () {
-    const emailField = document.getElementById('userEmail');
-    const email = emailField.value;
-    const passField = document.getElementById('userPass');
-    const pass = passField.value;
+//document.getElementById('login-btn').addEventListener('click', function () {
+//    const emailField = document.getElementById('userEmail');
+//    const email = emailField.value;
+//    const passField = document.getElementById('userPass');
+//    const pass = passField.value;
+//
+//    if (email === 'wajed@heaven.com' && pass === 'welcome2heaven') {
+//        location.href = 'bank.html'
+//    }
+//    else {
+//        alert('You are not a member of this Heaven, Rest in Peace')
+//    }
+//})
 
-    if (email === 'wajed@heaven.com' && pass === 'welcome2heaven') {
+// Login Page Script
+
+document.getElementById('login-btn').addEventListener('click', function () {
+    const emailField = document.getElementById('email-input').value;
+    const passField = document.getElementById('pass-input').value;
+    if (emailField === '' && passField === '') {
         location.href = 'bank.html'
     }
     else {
